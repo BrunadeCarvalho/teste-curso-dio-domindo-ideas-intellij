fcd45bcc7f759d2d468be1dffb9ad940e1b7cf86
Bruna de Carvalho <brunacarvalho260697@gmail.com>
2022-07-10 15:34:53 -0300

    Resolução segundo exercicio do curso Entendendo Métodos Java: Sobrecarga

---
+++ src/MetodosJava/Sobrecarga/Main.java
@@ -0,0 +1,14 @@
+package MetodosJava.Sobrecarga;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        // QUADRILÁTERO:
+        System.out.println("Exercicio quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5,5);
+        Quadrilatero.area(7, 8, 9);
+    }
+}
+ // DETERMINA QUAL É O CORRETO DE ACORDO COM O NÚMERO DE PARAMETROS SELE
